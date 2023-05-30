0328e68ef2de676813fb9eb0f6b3e980b280729e
aciel7 <131728772+aciel7@users.noreply.github.com>
2023-05-30 12:40:14 -0500

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -12,12 +12,20 @@ import tdt
 import matplotlib.pyplot as plt
 print("test")
 recording = si.read_tdt("/home/lambda/Desktop/Rats/Acute_052323-230523-171410/", stream_name="b'Wav2'")
-recording = si.read_tdt("/home/lambda/Desktop/Rats/Acute_052323-230523-171410/", stream_name="b'Wav2'")
+# recording = si.read_tdt("/home/lambda/Desktop/Rats/Acute_052323-230523-171410/", stream_name="b'Wav2'")
 
 #%%
 data = tdt.read_block("/home/lambda/Desktop/Rats/Acute_052323-230523-171410/Exp_BL-230523-213247")
 
 #%%
+recording_filt = spre.bandpass_filter(recording, 300, 6000)
+recording_cmr = spre.common_reference(recording_filt, reference = "global")
 
-w_ts = sw.plot_timeseries(recording, time_range=(0, 100), segment_index = 8)
 
+sorting = ss.run_sorter('kilosort2',
+    recording=recording_cmr,
+    output_folder="kilosort3",
+    singularity_image=True)
+
+
+# w_ts = sw.plot_timeseries(recording, time_range=(0, 50), segment_index = 8)
